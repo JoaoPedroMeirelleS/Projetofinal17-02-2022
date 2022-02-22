7984b49817dcf7bcceea06d06d5f2f4d22e11e5d
JoaoPedroMeirelleS <94013155+JoaoPedroMeirelleS@users.noreply.github.com>
2022-02-22 15:45:25 -0300

    22022022

--- Projetofinal/Projetofinal/home.aspx
+++ Projetofinal/Projetofinal/home.aspx
@@ -1,6 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="Projetofinal.home" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">  
+    <!-- script-->
+
 </asp:Content>  
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">  
+      <!--conteudo-->
+      <form id="Form1" method="post" runat="server" enctype="multipart/form-data" action="UploadDeArquivo.aspx">
+         Arquivo Enviado para o servidor: <input id="oFile" type="file" runat="server" name="oFile">
+         <asp:button id="btnUpload" type="submit" text="Upload" runat="server"></asp:button>
+         <asp:Panel ID="frmConfirmation" Visible="False" Runat="server">
+            <asp:Label id="lblUploadResult" Runat="server"></asp:Label>
+         </asp:Panel>
+      </form>
     <h1>Página Inicial</h1>  
 </asp:Content>  
--- Projetofinal/Projetofinal/home.aspx.designer.cs
+++ Projetofinal/Projetofinal/home.aspx.designer.cs
@@ -13,5 +13,50 @@ namespace Projetofinal
 
     public partial class home
     {
+
+        /// <summary>
+        /// Controle Form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
+
+        /// <summary>
+        /// Controle oFile.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile oFile;
+
+        /// <summary>
+        /// Controle btnUpload.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnUpload;
+
+        /// <summary>
+        /// Controle frmConfirmation.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel frmConfirmation;
+
+        /// <summary>
+        /// Controle lblUploadResult.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUploadResult;
     }
 }
